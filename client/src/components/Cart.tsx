c867020155c8f4b7d463bbc3a2f6dca564ca334f
sejal <sakharwsejal@gmail.com>
2026-05-24 22:56:22 +0530

    Cart Page and Checkout Popups ready

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,8 +1,90 @@
+import { Minus, Plus } from "lucide-react"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Button } from "./ui/button"
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "./ui/table"
+import { useState } from "react"
+import CheckoutConfirmPage from "./CheckoutConfirmPage"
 
 const Cart = () => {
+
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
-        <div>Cart</div>
+        <div className="flex flex-col max-w-7xl mx-auto my-10 px-1 md:p-4">
+            <div className="flex justify-end">
+                <Button className="md:w-1/8 w-25 bg-button hover:bg-hoverButtonColor">Clear All</Button>
+            </div>
+
+
+            <Table className="mt-6">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-center">Items</TableHead>
+                        <TableHead className="text-center">Title</TableHead>
+                        <TableHead className="text-center">Price</TableHead>
+                        <TableHead className="text-center">Quantity</TableHead>
+                        <TableHead className="text-center">Total</TableHead>
+                        <TableHead className="text-center">Remove</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                    <MyTableRow />
+                </TableBody>
+                <TableFooter>
+                    <TableCell colSpan={5} className="text-center font-extrabold">Total</TableCell>
+                    <TableCell className="font-extrabold">₹ 180</TableCell>
+                </TableFooter>
+            </Table>
+            <div className="flex justify-end my-5">
+                <Button onClick={() => { setOpen(!open) }} className="bg-button hover:bg-hoverButtonColor md:h-12">Proceed To Checkout</Button>
+            </div>
+            <CheckoutConfirmPage open={open} setOpen={setOpen} />
+        </div>
     )
 }
 
 export default Cart
+
+const MyTableRow = () => {
+    return <>
+        <TableRow>
+            <TableCell className="grid place-items-center">
+                <Avatar>
+                    <AvatarImage src="" alt="" />
+                    <AvatarFallback>CA</AvatarFallback>
+                </Avatar>
+            </TableCell>
+            <TableCell className="text-center" >Biryani</TableCell>
+            <TableCell className="text-center">₹90</TableCell>
+            <TableCell className="grid place-items-center">
+                <div className="w-fit flex justify-between gap-1 items-center rounded-full border border-gray-100 dark:border-gray-800 ">
+                    <Button size={'icon'} className="rounded-full" variant={'outline'}>
+                        <Minus className="" />
+                    </Button>
+                    <Button disabled size={'icon'} className="rounded-full" variant={'outline'}>
+                        2
+                    </Button>
+                    <Button size={'icon'} className="rounded-full" variant={'outline'}>
+                        <Plus className="" />
+                    </Button>
+                </div>
+            </TableCell>
+            <TableCell className="text-center">₹ 180</TableCell>
+            <TableCell className="flex justify-center items-center">
+                <div className="bg-button px-2 py-1 md:w-3/4 grid place-items-center rounded-sm">
+                    Remove
+                </div>
+            </TableCell>
+        </TableRow>
+    </>
+}
---
+++ client/src/components/CheckoutConfirmPage.tsx
@@ -0,0 +1,129 @@
+import { useState, type ChangeEvent, type Dispatch, type FormEvent, type SetStateAction } from "react"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } from "./ui/dialog"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import { Contact, Globe, LocationEdit, Mail, MapPinIcon, Phone, PinIcon, User } from "lucide-react"
+import { Button } from "./ui/button"
+
+
+
+const CheckoutConfirmPage = ({ open, setOpen }: { open: boolean, setOpen: Dispatch<SetStateAction<boolean>> }) => {
+
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        contact: "",
+        address: "",
+        city: "",
+        country: ""
+    })
+    const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setInput({ ...input, [e.target.name]: e.target.value })
+    }
+
+    const checkoutHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(input);
+
+    }
+    return (
+        <Dialog open={open} onOpenChange={setOpen}  >
+            <DialogContent className="sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
+                <DialogTitle className="text-center text-xl font-bold ">Review Your Order</DialogTitle>
+                <DialogDescription className="text-center text-sm text-button">
+                    Double-check your delivery details and ensure everything is in order.
+                    When you are ready. hit confirm button to finalize your order.
+                </DialogDescription>
+                <form onSubmit={checkoutHandler} className="md:grid  grid-cols-1  gap-2 space-y-1 md:space-y-0">
+                    <div className="md:grid  grid-cols-2  gap-2 space-y-1 md:space-y-0">
+
+                        <div className="flex flex-col gap-3" >
+                            <Label className="text-sm text-gray-700">
+                                <User className="text-gray-700 ml-1" />
+                                Name
+                            </Label>
+                            <Input
+                                type="text"
+                                name="name"
+                                value={input.name}
+                                onChange={changeEventHandler}
+                                className="focus-visible:ring-0 "
+                            />
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label className="text-sm text-gray-700">
+                                <Mail className="text-gray-700 ml-1" />
+                                Email
+                            </Label>
+                            <Input
+                                type="email"
+                                name="email"
+                                value={input.email}
+                                onChange={changeEventHandler}
+                                className="focus-visible:ring-0 "
+                            />
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label className="text-sm text-gray-700">
+                                <Phone className="text-gray-700 ml-1" />
+                                Contact
+                            </Label>
+                            <Input
+                                type="text"
+                                name="contact"
+                                value={input.contact}
+                                onChange={changeEventHandler}
+                                className="focus-visible:ring-0 "
+                            />
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label className="text-sm text-gray-700">
+                                <LocationEdit className="text-gray-700 ml-1" />
+                                Address
+                            </Label>
+                            <Input
+                                type="text"
+                                name="address"
+                                value={input.address}
+                                onChange={changeEventHandler}
+                                className="focus-visible:ring-0 "
+                            />
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label className="text-sm text-gray-700">
+                                <MapPinIcon className="text-gray-700 ml-1" />
+                                City
+                            </Label>
+                            <Input
+                                type="text"
+                                name="city"
+                                value={input.city}
+                                onChange={changeEventHandler}
+                                className="focus-visible:ring-0 "
+                            />
+                        </div>
+                        <div className="flex flex-col gap-3">
+                            <Label className="text-sm text-gray-700">
+                                <Globe className="text-gray-700 ml-1" />
+                                Country
+                            </Label>
+                            <Input
+                                type="text"
+                                name="country"
+                                value={input.country}
+                                onChange={changeEventHandler}
+                                className="focus-visible:ring-0 "
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit" className="bg-button hover:bg-hoverButtonColor sm:w-70 md:w-70 h-12 ">Continue To Payment</Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default CheckoutConfirmPage
