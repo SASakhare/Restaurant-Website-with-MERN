a39bc406439701086685903ace4d0ef8c91ed6ec
sejal <sakharwsejal@gmail.com>
2026-05-25 12:17:41 +0530

    admin restaurant page done with its schema

---
+++ client/src/admin/Restaurant.tsx
@@ -0,0 +1,173 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Loader2 } from "lucide-react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import { type RestaurantFormSchema, restaurantFormSchema } from "@/schema/RestaurantsSchema";
+
+
+
+const Restaurant = () => {
+
+    const loading = false;
+
+    const [input, setInput] = useState<RestaurantFormSchema>({
+        restaurantName: "",
+        city: "",
+        country: "",
+        deliveryTime: "",
+        cuisines: [],
+        imageFile: "",
+
+    })
+
+    const [errors, setErrors] = useState<Partial<RestaurantFormSchema>>({});
+
+    const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type } = e.target;
+
+        setInput({
+            ...input, [name]: type == 'number' ? (value == "" ? "" : Number(value)) : value,
+        })
+    }
+
+
+    const addrestaurantHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(input);
+
+        const result = restaurantFormSchema.safeParse(input)
+
+        if (!result.success) {
+
+            const fieldErrors = result.error.flatten().fieldErrors;
+            console.log(fieldErrors.cuisines);
+
+            setErrors({
+                restaurantName: fieldErrors.restaurantName?.[0] || "",
+                city: fieldErrors.city?.[0] || "",
+                country: fieldErrors.country?.[0] || "",
+                deliveryTime: fieldErrors.deliveryTime?.[0] || "",
+                cuisines: fieldErrors.cuisines?.[0] || "",
+                imageFile: fieldErrors.imageFile?.[0] || "",
+            } as unknown as Partial<RestaurantFormSchema>)
+
+            // console.log(errors ? "true" : "false");
+
+        } else {
+            setErrors({});
+        }
+
+        console.log(errors);
+
+    }
+
+    return (
+        <div className="max-w-6xl mx-auto my-10 p-2">
+            <div>
+                <div>
+                    <h1 className="font-extrabold text-2xl mb-5">Add Restaurant</h1>
+                    <form action="" onSubmit={addrestaurantHandler} className="flex flex-col gap-10 w-full">
+                        <div className="md:grid grid-cols-2 gap-6 space-y-2 md:space-y-0">
+                            {/* Restaurant Name */}
+                            <div className="flex flex-col gap-2">
+                                <Label className="text-gray-700">Restaurant Name</Label>
+                                <Input
+                                    type="text"
+                                    name="restaurantName"
+                                    value={input.restaurantName}
+                                    onChange={changeEventHandler}
+                                    placeholder="Enter your restaurant name"
+                                />
+                                {
+                                    errors.restaurantName && <span className="text-xs text-red-600 font-medium">{errors.restaurantName} </span>
+                                }
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <Label className="text-gray-700">City</Label>
+                                <Input
+                                    type="text"
+                                    name="city"
+                                    value={input.city}
+                                    onChange={changeEventHandler}
+                                    placeholder="Enter your City name"
+                                />
+                                {
+                                    errors.city && <span className="text-xs text-red-600 font-medium">{errors.city} </span>
+                                }
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <Label className="text-gray-700">Country</Label>
+                                <Input
+                                    type="text"
+                                    name="country"
+                                    value={input.country}
+                                    onChange={changeEventHandler}
+                                    placeholder="Enter your country name"
+                                />
+                                {
+                                    errors.country && <span className="text-xs text-red-600 font-medium">{errors.country} </span>
+                                }
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <Label className="text-gray-700">Delivery Time</Label>
+                                <Input
+                                    type="number"
+                                    name="deliveryTime"
+                                    value={input.deliveryTime}
+                                    onChange={changeEventHandler}
+                                    placeholder="Enter your Delivery Time"
+                                />
+                                {
+                                    errors.deliveryTime && <span className="text-xs text-red-600 font-medium">{errors.deliveryTime} </span>
+                                }
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <Label className="text-gray-700">Cuisines</Label>
+                                <Input
+                                    type="text"
+                                    name="cuisines"
+                                    value={input.cuisines}
+                                    onChange={(e) => setInput({ ...input, cuisines: e.target.value.split(',') })}
+                                    placeholder="eg. Momos,Biryani .."
+                                />
+                                {
+                                    errors.cuisines && <span className="text-xs text-red-600 font-medium">{"cuisines is required."} </span>
+                                }
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <Label className="text-gray-700">Upload Restaurant Banner</Label>
+                                <Input
+                                    type="file"
+                                    accept="image/*"
+                                    // value={input.imageFile?.}
+                                    onChange={(e) => {
+                                        setInput({
+                                            ...input, [e.target.name]: e.target.files?.[0] || undefined
+                                        })
+                                    }}
+                                    name="imageFile"
+                                    placeholder="Upload Restaurant Banner"
+                                />
+                                {
+                                    errors.imageFile && <span className="text-xs text-red-600 font-medium">{errors.imageFile} </span>
+                                }
+                            </div>
+                        </div>
+                        <div className="w-full flex items-center justify-center">
+                            {
+                                loading ? (
+                                    <Button disabled className=" w-full sm:w-1/2  md:w-1/3 bg-button hover:bg-hoverButtonColor"><Loader2 className="animate-spin text-2xl" />  Please Wait</Button>
+                                ) : (
+                                    <Button type="submit" className=" w-full sm:w-1/2  md:w-1/3 bg-button hover:bg-hoverButtonColor">Add Restaurant</Button>
+                                )
+                            }
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Restaurant
