1f09fd007280fc2bf8b8a52bd1da971a91683887
sejal <sakharwsejal@gmail.com>
2026-05-26 15:42:40 +0530

    backend setup completed

---
+++ server/src/db/connectDB.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string)
+        console.log("mongoDB connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectDB;
+
+
+
+
+
+
+
+
+
+
