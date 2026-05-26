098f65cee5e717ed1fc0d46f53a0ee48e162425c
sejal <sakharwsejal@gmail.com>
2026-05-26 18:10:59 +0530

    all models and schema and define and build

---
+++ server/src/models/order.model.ts
@@ -0,0 +1,133 @@
+import mongoose, { Document } from "mongoose";
+
+type DeliveryDetails = {
+    email: string,
+    name: string,
+    address: string,
+    city: string,
+}
+
+type CartItems = {
+    menuId: string,
+    name: string,
+    image: string,
+    price: number,
+    quantity: number
+}
+
+export interface IOrder {
+    user: mongoose.Schema.Types.ObjectId,
+    restaurant: mongoose.Schema.Types.ObjectId,
+    deliveryDetails: DeliveryDetails,
+    cartItems: CartItems[],
+    totalAmount: number,
+    status: "pending" | "confirmed" | "preparing" | "outfordelivery" | "delivered"
+}
+
+
+export interface IOrderDocument extends IOrder, Document {
+    createdAt: Date,
+    updatedAt: Date,
+
+}
+
+
+export const OrderSchema = new mongoose.Schema<IOrderDocument>({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    restaurant: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Restaurant"
+    },
+    deliveryDetails: {
+        email: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        }
+    },
+    cartItems: [{
+        menuId: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        }
+    }],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "confirmed", "preparing", "outfordelivery", "delivered"],
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+
+
+
+
+export const Order = mongoose.model("Order", OrderSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
