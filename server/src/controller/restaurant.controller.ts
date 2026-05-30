bd1135501fd7a6bf1e03ffd693eaddd5631f7ce8
sejal <sakharwsejal@gmail.com>
2026-05-30 18:48:22 +0530

    feat(search): add server-side search, filtering, and query processing

--- server/src/controller/restaurant.controller.ts
+++ server/src/controller/restaurant.controller.ts
@@ -211,7 +211,6 @@ export const searchRestaurant = async (req: Request, res: Response) => {
 
         const selectedCuisines = (req.query.selectedCuisines as string || "").split(",").filter(cuisine => cuisine).map((cuisine) => cuisine.toLowerCase())
 
-
         const query: any = {};
 
         // * basic search based on searchText (name,city,country)
@@ -233,7 +232,6 @@ export const searchRestaurant = async (req: Request, res: Response) => {
             ]
         }
 
-        // console.log(query);
 
         if (selectedCuisines.length > 0) {
             query.cuisines = { $in: selectedCuisines }
